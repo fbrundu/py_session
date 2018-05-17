a3d8f95dde580db3820ba5404e7ec6fd1fdca49f
Francesco Brundu <francesco.brundu@gmail.com>
2018-05-17 16:47:00 -0400

    Sorted list

--- py_session/__init__.py
+++ py_session/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Francesco G. Brundu"""
 __email__ = 'francesco.brundu@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .py_session import py_session
--- py_session/py_session.py
+++ py_session/py_session.py
@@ -4,24 +4,38 @@
 
 import sys
 
-def py_session(include_na=False):
+def py_session(include_na=False, return_list=False):
 
     modules_print = []
     for module in sys.modules:
         if '.' not in module and not module.startswith('_'):
             try:
-                modules_print += [f'{module:15}\t{sys.modules[module].__version__}']
+                modules_print += [f'{module:20}\t{sys.modules[module].__version__}']
             except:
                 try:
                     if type(modules[module].version) is str:
-                        modules_print += [f'{module:15}\t{sys.modules[module].version}']
+                        modules_print += [f'{module:20}\t{sys.modules[module].version}']
                     else:
-                        modules_print += [f'{module:15}\t{sys.modules[module].version()}']
+                        modules_print += [f'{module:20}\t{sys.modules[module].version()}']
                 except:
                     try:
-                        modules_print += [f'{module:15}\t{sys.modules[module].VERSION}']
+                        modules_print += [f'{module:20}\t{sys.modules[module].VERSION}']
                     except:
                         if include_na:
-                            modules_print += [f'{module:15}\tNA']
-    for a,b,c in zip(modules_print[::3], modules_print[1::3], modules_print[2::3]):
-        print(f'{a:30}{b:30}{c}')
+                            modules_print += [f'{module:20}\tNA']
+    modules_print = sorted(modules_print)
+    modules_len = len(modules_print)
+    print(f'{modules_len} modules found')
+    col_len = modules_len // 3
+    add = modules_len % 3
+    if add > 0:
+        col_len += 1
+    for a,b,c in zip(modules_print[:col_len],
+                     modules_print[col_len:2*col_len],
+                     modules_print[2*col_len:]):
+        print(f'{a:45}{b:45}{c:}')
+    if add > 0:
+        for i in range(add):
+            print(f'{modules_print[(i+1)*col_len-1]:45}', end='')
+    if return_list:
+        return modules_print
